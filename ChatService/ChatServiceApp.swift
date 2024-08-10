501ed5d7fb4209412c6d2f3d4dbe3e83fe652ea9
GSM1900\kstjohn2 <kevin.stjohn2@t-mobile.com>
2024-08-09 17:03:54 -0700

    Initial Commit

---
+++ ChatService/ChatServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatServiceApp.swift
+//  ChatService
+//
+//  Created by ksj on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
