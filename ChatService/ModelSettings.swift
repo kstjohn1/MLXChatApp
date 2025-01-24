85e0ac9df9e287e98ce248ff2f5b1291298e4c9c
Kevin St John <kstjohn@kstjohn.net>
2025-01-24 10:21:03 -0800

    Update defaults

--- ChatService/ModelSettings.swift
+++ ChatService/ModelSettings.swift
@@ -16,10 +16,10 @@ class ModelSettings: ObservableObject {
     @Published var systemMessage: String
     
     init(
-        temperature: Double = UserDefaults.standard.double(forKey: "temperature"),
-        topP: Double = UserDefaults.standard.double(forKey: "topP"),
-        maxTokens: Int = UserDefaults.standard.integer(forKey: "maxTokens"),
-        stream: Bool = UserDefaults.standard.bool(forKey: "stream"),
+        temperature: Double = UserDefaults.standard.double(forKey: "temperature") != 0 ? UserDefaults.standard.double(forKey: "temperature") : 0.6,
+        topP: Double = UserDefaults.standard.double(forKey: "topP") != 0 ? UserDefaults.standard.double(forKey: "topP") : 0.9,
+        maxTokens: Int = UserDefaults.standard.integer(forKey: "maxTokens") != 0 ? UserDefaults.standard.integer(forKey: "maxTokens") : 32000,
+        stream: Bool = !UserDefaults.standard.bool(forKey: "stream") ? false : true,
         systemMessage: String = UserDefaults.standard.string(forKey: "systemMessage") ?? ""
     ) {
         self.temperature = temperature
@@ -29,5 +29,3 @@ class ModelSettings: ObservableObject {
         self.systemMessage = systemMessage
     }
 }
-
-
